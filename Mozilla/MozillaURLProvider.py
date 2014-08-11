832f89901b8091588e1236115a84b1fc9fdef45e
Greg Neagle <gregneagle@mac.com>
2014-08-11 12:19:34 -0700

    Pylint cleanups for MozillaURLProvider processor

--- Mozilla/MozillaURLProvider.py
+++ Mozilla/MozillaURLProvider.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+"""See docstring for MozillaURLProvider class"""
 
 import re
 import urllib2
@@ -26,26 +26,28 @@ __all__ = ["MozillaURLProvider"]
 
 MOZ_BASE_URL = "http://ftp.mozilla.org/pub/mozilla.org/"
                #"firefox/releases")
-re_dmg = re.compile(r'a[^>]* href="(?P<filename>[^"]+\.dmg)"')
+RE_DMG = re.compile(r'a[^>]* href="(?P<filename>[^"]+\.dmg)"')
 
 
 class MozillaURLProvider(Processor):
-    description = "Provides URL to the latest Firefox release."
+    """Provides URL to the latest Firefox release."""
+    description = __doc__
     input_variables = {
         "product_name": {
             "required": True,
-            "description": 
+            "description":
                 "Product to fetch URL for. One of 'firefox', 'thunderbird'.",
         },
         "release": {
             "required": False,
-            "description": ("Which release to download. Examples: 'latest', "
+            "description": (
+                "Which release to download. Examples: 'latest', "
                 "'latest-10.0esr', 'latest-esr', 'latest-beta'."),
         },
         "locale": {
             "required": False,
-            "description": 
-                    "Which localization to download, default is 'en_US'.",
+            "description":
+                "Which localization to download, default is 'en_US'.",
         },
         "base_url": {
             "required": False,
@@ -57,53 +59,54 @@ class MozillaURLProvider(Processor):
             "description": "URL to the latest Mozilla product release.",
         },
     }
-    
-    __doc__ = description
-    
+
     def get_mozilla_dmg_url(self, base_url, product_name, release, locale):
+        """Get download URL for Mozilla product"""
+        #pylint: disable=no-self-use
         # Allow locale as both en-US and en_US.
         locale = locale.replace("_", "-")
-        
+
         # Construct download directory URL.
         release_dir = release.lower()
-        
+
         index_url = "/".join(
             (base_url, product_name, "releases", release_dir, "mac", locale))
         #print >>sys.stderr, index_url
-        
+
         # Read HTML index.
         try:
-            f = urllib2.urlopen(index_url)
-            html = f.read()
-            f.close()
-        except BaseException as e:
-            raise ProcessorError("Can't download %s: %s" % (index_url, e))
-        
+            fref = urllib2.urlopen(index_url)
+            html = fref.read()
+            fref.close()
+        except BaseException as err:
+            raise ProcessorError("Can't download %s: %s" % (index_url, err))
+
         # Search for download link.
-        m = re_dmg.search(html)
-        if not m:
+        match = RE_DMG.search(html)
+        if not match:
             raise ProcessorError(
-                "Couldn't find %s download URL in %s" 
+                "Couldn't find %s download URL in %s"
                 % (product_name, index_url))
-        
+
         # Return URL.
         return "/".join(
             (base_url, product_name, "releases", release_dir, "mac", locale,
-             m.group("filename")))
-    
+             match.group("filename")))
+
     def main(self):
+        """Provide a Mozilla download URL"""
         # Determine product_name, release, locale, and base_url.
         product_name = self.env["product_name"]
         release = self.env.get("release", "latest")
         locale = self.env.get("locale", "en_US")
         base_url = self.env.get("base_url", MOZ_BASE_URL)
-        
+
         self.env["url"] = self.get_mozilla_dmg_url(
-                                        base_url, product_name, release, locale)
+            base_url, product_name, release, locale)
         self.output("Found URL %s" % self.env["url"])
-    
+
 
 if __name__ == "__main__":
-    processor = MozillaURLProvider()
-    processor.execute_shell()
-    
+    PROCESSOR = MozillaURLProvider()
+    PROCESSOR.execute_shell()
+
