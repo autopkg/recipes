5f34a604ccb6d03e787a440f17e3a6897efe0f19
Greg Neagle <gregneagle@mac.com>
2014-08-11 15:44:17 -0700

    Missing commit for Pylint cleanups for MunkitoolsURLProvider processor

--- munkitools/MunkitoolsURLProvider.py
+++ munkitools/MunkitoolsURLProvider.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""See docstring for MunkitoolsURLProvider processor"""
 
 import re
 import urllib2
@@ -29,12 +30,15 @@ MUNKIBUILDS_URLS = ["http://munkibuilds.org/munkitools-latest.dmg",
                     "http://munkibuilds.org/munkitools2-latest.pkg",
                     "https://munkibuilds.org/munkitools2-latest.pkg"]
 DEFAULT_MAJOR_VERSION = "1"
-re_dmg_link = re.compile(r'href="(?P<url>//munki.googlecode.com/files/munkitools-[.0-9]*.dmg)"')
+RE_DMG_LINK = re.compile(
+    r'href="(?P<url>//munki.googlecode.com/files/munkitools-[.0-9]*.dmg)"')
 # placeholder for an 're_pkg_link' for Munki 2:
-# re_pkg_link = re.compile(r'href="(?P<url>//munki.googlecode.com/files/munkitools-[.0-9]*.pkg)"')
+# RE_PKG_LINK = re.compile(
+#    r'href="(?P<url>//munki.googlecode.com/files/munkitools-[.0-9]*.pkg)"')
 
 class MunkitoolsURLProvider(Processor):
     """Provides a download URL for Munki tools."""
+    description = __doc__
     input_variables = {
         "base_url": {
             "required": False,
@@ -43,8 +47,8 @@ class MunkitoolsURLProvider(Processor):
         "major_version": {
             "required": False,
             "description": ("Major version of Munki tools to download. "
-                            "Either '1' or '2', defaults to %s." %
-                            DEFAULT_MAJOR_VERSION)
+                            "Either '1' or '2', defaults to %s."
+                            % DEFAULT_MAJOR_VERSION)
         },
     }
     output_variables = {
@@ -52,28 +56,28 @@ class MunkitoolsURLProvider(Processor):
             "description": "URL to Munkitools disk image download.",
         },
     }
-    description = __doc__
 
-    def get_munkitools_dmg_url(self, base_url, munki_version):
+    def get_munkitools_dmg_url(self, base_url):
+        """Get data from URL and return a download URL"""
+        #pylint: disable=no-self-use
         # Read HTML index.
         try:
-            f = urllib2.urlopen(base_url)
-            html = f.read()
-            f.close()
+            fref = urllib2.urlopen(base_url)
+            html = fref.read()
+            fref.close()
         except BaseException as err:
             raise ProcessorError("Can't download %s: %s" % (base_url, err))
-        
-        m = re_dmg_link.search(html)
-        
-        if not m:
+
+        match = RE_DMG_LINK.search(html)
+
+        if not match:
             raise ProcessorError(
                 "Couldn't find munkitools download URL in %s" % base_url)
-        
-        link = urllib2.quote(m.group("url"), safe=":/%")
+
+        link = urllib2.quote(match.group("url"), safe=":/%")
         if link.startswith("//"):
             link = "http:" + link
         return link
-        
 
     def main(self):
         """Find and return a download URL"""
@@ -81,16 +85,15 @@ class MunkitoolsURLProvider(Processor):
         major_version = self.env.get("major_version", DEFAULT_MAJOR_VERSION)
         if major_version != "1" and base_url == RELEASE_BASE_URL:
             raise ProcessorError(
-                ("This processor currently only supports a 'major_version' "
-                 "of '1' unless a munkibuilds.org URL is given as a 'base_url'."))
+                "This processor currently only supports a 'major_version' "
+                "of '1' unless a munkibuilds.org URL is given as a 'base_url'.")
         if base_url in MUNKIBUILDS_URLS:
             self.env["url"] = base_url
         else:
-            self.env["url"] = self.get_munkitools_dmg_url(base_url,
-                                                          major_version)
+            self.env["url"] = self.get_munkitools_dmg_url(base_url)
         self.output("Found URL %s" % self.env["url"])
 
 
 if __name__ == "__main__":
-    processor = MunkitoolsURLProvider()
-    processor.execute_shell()
+    PROCESSOR = MunkitoolsURLProvider()
+    PROCESSOR.execute_shell()
