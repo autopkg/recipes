d5fac3df8b0bed1922b221af5e16863aa2d67845
Greg Neagle <gregneagle@mac.com>
2014-08-11 10:16:44 -0700

    PyLint cleanups for AdobeAcrobatProUpdateInfoProvider processor

--- AdobeAcrobatPro/AdobeAcrobatProUpdateInfoProvider.py
+++ AdobeAcrobatPro/AdobeAcrobatProUpdateInfoProvider.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+"""See docstring for AdobeAcrobatProUpdateInfoProvider class"""
 
 import urllib2
 import re
@@ -31,28 +31,30 @@ META_BASE_URL = "https://armmf.adobe.com/arm-manifests/mac"
 MANIFEST_URL_TEMPLATE = META_BASE_URL + "/{MAJREV}/manifest_url_template.txt"
 DL_BASE_URL = "http://armdl.adobe.com"
 
-_url_vars = {
+_URL_VARS = {
     "PROD": "com_adobe_Acrobat_Pro",
     "PROD_ARCH": "univ"
 }
-supported_vers = ['9', '10', '11']
+SUPPORTED_VERS = ['9', '10', '11']
 
 class AdobeAcrobatProUpdateInfoProvider(Processor):
-    description = "Provides URL to the latest Adobe Acrobat Pro release."
+    """Provides URL to the latest Adobe Acrobat Pro release."""
+    description = __doc__
     input_variables = {
         "major_version": {
             "required": True,
             "description": ("Major version. Currently supports: %s"
-                            % ", ".join(supported_vers))
+                            % ", ".join(SUPPORTED_VERS))
         },
         "version": {
             "required": False,
-            "description": "Update version number. Defaults to %s." % VERSION_DEFAULT,
+            "description": ("Update version number. Defaults to %s."
+                            % VERSION_DEFAULT),
         },
         "munki_update_name": {
             "required": False,
-            "description": ("Name for the update in Munki. Defaults to "
-                            "%s" % MUNKI_UPDATE_NAME_DEFAULT)
+            "description": ("Name for the update in Munki. Defaults to %s"
+                            % MUNKI_UPDATE_NAME_DEFAULT)
         }
     }
     output_variables = {
@@ -63,86 +65,98 @@ class AdobeAcrobatProUpdateInfoProvider(Processor):
             "description": "Version for this update.",
         },
         "additional_pkginfo": {
-            "description": ("A pkginfo possibly containing additional 'requires' items.")
+            "description":
+                "A pkginfo possibly containing additional 'requires' items."
         }
     }
 
-    __doc__ = description
-
     def process_url_vars(self, url):
-        for var in _url_vars.keys():
-            subbed_url = url.replace(r"{%s}" % var, _url_vars[var])
+        '''Substitute keys in URL templates with actual values'''
+        #pylint: disable=no-self-use
+        for var in _URL_VARS.keys():
+            subbed_url = url.replace(r"{%s}" % var, _URL_VARS[var])
             url = subbed_url
         return subbed_url
 
     def get_url_response(self, url):
+        '''Get data from a url'''
+        #pylint: disable=no-self-use
         try:
             url_handle = urllib2.urlopen(url)
             response = url_handle.read()
             url_handle.close()
-        except BaseException as e:
-            raise ProcessorError("Can't read response from URL %s: %s" % (url, e))
+        except BaseException as err:
+            raise ProcessorError(
+                "Can't read response from URL %s: %s" % (url, err))
         return response
 
     def get_manifest_data(self, manifest_plist_url):
+        '''Get manifest(plist) data from a url'''
         manifest_plist_response = self.get_url_response(manifest_plist_url)
         try:
             manifest_data = readPlistFromString(manifest_plist_response)
-        except BaseException as e:
-            raise ProcessorError("Can't parse manifest plist at %s: %s" % (manifest_plist_url, e))
+        except BaseException as err:
+            raise ProcessorError(
+                "Can't parse manifest plist at %s: %s"
+                % (manifest_plist_url, err))
 
         if "PatchURL" not in manifest_data.keys():
             raise ProcessorError("Manifest plist key '%s' not found at %s: %s"
-                                 % ("PatchURL", manifest_plist_url, e))
+                                 % ("PatchURL", manifest_plist_url, err))
 
         return manifest_data
 
     def get_acrobat_metadata(self, major_version, get_version):
         '''Returns a tuple: (url, version, previous_required_version)'''
-
+        # FIXME: major_version parameter is not currently used.
         template_url = self.process_url_vars(MANIFEST_URL_TEMPLATE)
         template_response = self.get_url_response(template_url)
 
         if get_version != "latest":
             # /{MAJREV}/latest/{PROD}_{PROD_ARCH}.plist -->
             # /{MAJREV}/get_version/{PROD}_{PROD_ARCH}.plist
-            template_response = re.sub("\d+\.\d+\.\d+", get_version, template_response)
-        
+            template_response = re.sub(
+                r"\d+\.\d+\.\d+", get_version, template_response)
+
         manifest_url = self.process_url_vars(META_BASE_URL + template_response)
         manifest_data = self.get_manifest_data(manifest_url)
 
 
         composed_dl_url = DL_BASE_URL + manifest_data["PatchURL"]
         version = manifest_data["BuildNumber"]
-        # If there's a previous required version, store that version for later use
-        if manifest_data.get("PreviousURLTemplate","") != "noTemplate":
+        # If there's a previous required version,
+        # store that version for later use
+        if manifest_data.get("PreviousURLTemplate", "") != "noTemplate":
             prev_manifest_url = self.process_url_vars(
-                                META_BASE_URL + manifest_data["PreviousURLTemplate"])
+                META_BASE_URL + manifest_data["PreviousURLTemplate"])
             prev_manifest_data = self.get_manifest_data(prev_manifest_url)
             prev_version = prev_manifest_data["BuildNumber"]
-        return (composed_dl_url,version,prev_version)
+        return (composed_dl_url, version, prev_version)
 
     def main(self):
+        '''Do our processor task!'''
         major_version = self.env["major_version"]
         get_version = self.env.get("version", VERSION_DEFAULT)
-        if major_version not in supported_vers:
-            raise ProcessorError("major_version %s not one of those supported: %s"
-                                % (major_version, ", ".join(supported_vers)))
+        if major_version not in SUPPORTED_VERS:
+            raise ProcessorError(
+                "major_version %s not one of those supported: %s"
+                % (major_version, ", ".join(SUPPORTED_VERS)))
 
-        global _url_vars
-        _url_vars["MAJREV"] = major_version
+        #global _URL_VARS global statement not needed to modify a key/value pair
+        _URL_VARS["MAJREV"] = major_version
 
         munki_update_name = self.env.get("munki_update_name", "")
         if not munki_update_name:
             munki_update_name = self.process_url_vars(MUNKI_UPDATE_NAME_DEFAULT)
-        (url, version, prev_version) = self.get_acrobat_metadata(major_version,
-                                                                get_version=get_version)
+        (url, version, prev_version) = self.get_acrobat_metadata(
+            major_version, get_version=get_version)
 
         new_pkginfo = {}
         # if our required version is something other than a base version
         # should match a version ending in '.0.0', '.00.0', '.00.00', etc.
-        if not re.search("\.[0]+\.[0]+", prev_version):
-            new_pkginfo["requires"] = ["%s-%s" % (munki_update_name, prev_version)]
+        if not re.search(r"\.[0]+\.[0]+", prev_version):
+            new_pkginfo["requires"] = ["%s-%s"
+                                       % (munki_update_name, prev_version)]
             self.output("Update requires previous version: %s" % prev_version)
         self.env["additional_pkginfo"] = new_pkginfo
 
@@ -151,5 +165,5 @@ class AdobeAcrobatProUpdateInfoProvider(Processor):
         self.output("Found URL %s" % self.env["url"])
 
 if __name__ == "__main__":
-    processor = AdobeAcrobatProUpdateInfoProvider()
-    processor.execute_shell()
+    PROCESSOR = AdobeAcrobatProUpdateInfoProvider()
+    PROCESSOR.execute_shell()
