7cd3b79e9295e69ae9d8b02f8e0abcc5ac71857b
Greg Neagle <gregneagle@mac.com>
2014-08-11 16:01:22 -0700

    Pylint cleanups for PraatURLProvider

--- Praat/PraatURLProvider.py
+++ Praat/PraatURLProvider.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+"""See docstring for PraatURLProvider class"""
 
 import re
 import urllib2
@@ -30,12 +30,13 @@ PRAAT_DMG_RE = r'a href="?(?P<url>praat\d+_mac{0}\.dmg)"?'
 
 
 class PraatURLProvider(Processor):
-    description = "Provides URL to the latest release of Praat."
+    """Provides URL to the latest release of Praat."""
+    description = __doc__
     input_variables = {
         "arch_edition": {
             "required": False,
-            "description": ("Build architecture to retrieve. Can be either '32' or ",
-                            "64. Default is %s" % PRAAT_DEFAULT_ARCH),
+            "description": ("Build architecture to retrieve. Can be either ",
+                            "'32' or '64'. Default is %s" % PRAAT_DEFAULT_ARCH),
         },
         "base_url": {
             "required": False,
@@ -47,39 +48,37 @@ class PraatURLProvider(Processor):
             "description": "URL to the latest release of Praat.",
         },
     }
-    
-    __doc__ = description
-    
+
     def get_praat_dmg_url(self, base_url):
+        """Find the download URL in the HTML returned from the base_url"""
         arch = self.env.get('arch_edition', '32')
         re_praat_dmg = re.compile(PRAAT_DMG_RE.format(arch))
         # Read HTML index.
         try:
-            f = urllib2.urlopen(base_url)
-            html = f.read()
-            f.close()
-        except BaseException as e:
-            raise ProcessorError("Can't download %s: %s" % (base_url, e))
-        
+            fref = urllib2.urlopen(base_url)
+            html = fref.read()
+            fref.close()
+        except BaseException as err:
+            raise ProcessorError("Can't download %s: %s" % (base_url, err))
+
         # Search for download link.
-        m = re_praat_dmg.search(html)
-        if not m:
-            raise ProcessorError("Couldn't find Praat download URL in %s" % base_url)
-        
+        match = re_praat_dmg.search(html)
+        if not match:
+            raise ProcessorError(
+                "Couldn't find Praat download URL in %s" % base_url)
+
         # Return URL.
-        url = PRAAT_BASE_URL.rsplit("/", 1)[0] + "/" + m.group("url")
+        url = PRAAT_BASE_URL.rsplit("/", 1)[0] + "/" + match.group("url")
         return url
-    
+
     def main(self):
         # Determine base_url.
         base_url = self.env.get('base_url', PRAAT_BASE_URL)
-        
+
         self.env["url"] = self.get_praat_dmg_url(base_url)
         self.output("Found URL %s" % self.env["url"])
-    
+
 
 if __name__ == '__main__':
-    processor = PraatURLProvider()
-    processor.execute_shell()
-    
-
+    PROCESSOR = PraatURLProvider()
+    PROCESSOR.execute_shell()
