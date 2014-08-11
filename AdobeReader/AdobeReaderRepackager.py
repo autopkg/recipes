f2a915ba3b74491b1d4a1bb26441072d05f0a060
Greg Neagle <gregneagle@mac.com>
2014-08-11 11:57:50 -0700

    PyLint cleanups for AdobeReaderRepackager and AdobeReaderURLProvider processors; fix undefined variable in AdobeReaderRepackager

--- AdobeReader/AdobeReaderRepackager.py
+++ AdobeReader/AdobeReaderRepackager.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""See docstring for AdobeReaderRepackager class"""
 
 import os
 import shutil
@@ -20,20 +21,20 @@ import subprocess
 from xml.etree import ElementTree
 
 from autopkglib.DmgMounter import DmgMounter
-from autopkglib import Processor, ProcessorError
+from autopkglib import ProcessorError
 
 
 __all__ = ["AdobeReaderRepackager"]
 
 
 class AdobeReaderRepackager(DmgMounter):
-    
+
     # Modifies the Adobe Reader installer pkg so that:
     #
-    # 1) preinstall script does not interfere with installation when installed 
-    #    by root (for example, with Munki) -- the included preinstall script has 
+    # 1) preinstall script does not interfere with installation when installed
+    #    by root (for example, with Munki) -- the included preinstall script has
     #    a bunch of unneeded stuff in it and attempts to move any currently
-    #    installed copy of Adobe Reader into the current user's trash before  
+    #    installed copy of Adobe Reader into the current user's trash before
     #    install. We replace this with a simple script that just does an
     #    rm -r $install_location/Adobe Reader.app
     #
@@ -43,13 +44,13 @@ class AdobeReaderRepackager(DmgMounter):
     #    non-postponed install.
     #
     #    The postinstall script decompresses several files inside the
-    #    Adobe Reader.app application bundle. In the past, this was another 
-    #    thing preventing install on non-boot volumes. But testing with the 
-    #    11.0.3 installer pkg seems to indicate that the postinstall script and 
+    #    Adobe Reader.app application bundle. In the past, this was another
+    #    thing preventing install on non-boot volumes. But testing with the
+    #    11.0.3 installer pkg seems to indicate that the postinstall script and
     #    decompress utility does the right thing on non-boot volumes, so nothing
     #    special needs to be done with this.
     #
-    #    One could argue that it would make for a "neater" package to eliminate 
+    #    One could argue that it would make for a "neater" package to eliminate
     #    the postinstall script and the decompress bit; I'm taking the position
     #    of doing the minimum required to make the package work in the required
     #    scenarios.
@@ -69,22 +70,23 @@ class AdobeReaderRepackager(DmgMounter):
     # Though the AdobeReaderURLProvider can get Reader 10 as well as 11, it
     # is unlikely that this provider can properly repackage Reader 10.
     #
-
-    description = "Mounts an Adobe Reader XI install dmg and repackages the AdobeReader.pkg for automated deployment."
+    """Mounts an Adobe Reader XI install dmg and repackages the AdobeReader.pkg
+    for automated deployment."""
+    description = __doc__
     input_variables = {
         "dmg_path": {
             "required": True,
-            "description": "Path to a dmg containing the Adobe Reader installer.",
+            "description":
+                "Path to a dmg containing the Adobe Reader installer.",
         },
     }
     output_variables = {
         "pkg_path": "Path to the repackaged package."
     }
 
-    __doc__ = description
-
     def find_pkg(self, dir_path):
         '''Return path to the first package in dir_path'''
+        #pylint: disable=no-self-use
         for item in os.listdir(dir_path):
             if item.endswith(".pkg"):
                 return os.path.join(dir_path, item)
@@ -93,11 +95,12 @@ class AdobeReaderRepackager(DmgMounter):
 
     def expand(self, pkg, expand_dir):
         '''Uses pkgutil to expand a flat package.'''
+        #pylint: disable=no-self-use
         if os.path.isdir(expand_dir):
             try:
                 shutil.rmtree(expand_dir)
             except (OSError, IOError), err:
-                raise ProccessorError(
+                raise ProcessorError(
                     "Can't remove %s: %s" % (expand_dir, err))
         try:
             subprocess.check_call(
@@ -108,11 +111,12 @@ class AdobeReaderRepackager(DmgMounter):
 
     def flatten(self, expanded_pkg, destination):
         '''Flatten an expanded flat pkg'''
+        #pylint: disable=no-self-use
         if os.path.exists(destination):
             try:
                 os.unlink(destination)
             except OSError, err:
-                raise ProccessorError(
+                raise ProcessorError(
                     "Can't remove %s: %s" % (destination, err))
         try:
             subprocess.check_call(
@@ -122,8 +126,9 @@ class AdobeReaderRepackager(DmgMounter):
                 "%s flattening %s" % (err, expanded_pkg))
 
     def modify_distribution(self, expanded_pkg):
-        '''Modify the package Distribution file so that installation is allowed 
+        '''Modify the package Distribution file so that installation is allowed
         on non-boot volumes.'''
+        #pylint: disable=no-self-use
         dist_file = os.path.join(expanded_pkg, "Distribution")
         if not os.path.exists(dist_file):
             raise ProcessorError("%s not found")
@@ -131,7 +136,7 @@ class AdobeReaderRepackager(DmgMounter):
             dist = ElementTree.parse(dist_file)
         except (OSError, IOError, ElementTree.ParseError), err:
             raise ProcessorError("Can't read %s: %s" % (dist_file, err))
-            
+
         dist_root = dist.getroot()
         if not dist_root.tag in ["installer-script", "installer-gui-script"]:
             raise ProcessorError(
@@ -151,7 +156,8 @@ class AdobeReaderRepackager(DmgMounter):
         if not os.path.exists(app_pkg):
             raise ProcessorError("application.pkg not found!")
         preinstall_script = os.path.join(app_pkg, "Scripts/preinstall")
-        our_script = os.path.join(os.path.dirname(__file__),
+        our_script = os.path.join(
+            os.path.dirname(__file__),
             "package_resources/scripts/preinstall")
         if not os.path.exists(our_script):
             raise ProcessorError("%s not found" % our_script)
@@ -159,13 +165,13 @@ class AdobeReaderRepackager(DmgMounter):
             shutil.copy(our_script, preinstall_script)
         except (OSError, IOError), err:
             raise ProcessorError("%s copying %s" % (err, preinstall_script))
-        self.output("Replaced pkg preinstall script with our custom script at %s" %
-            our_script)
+        self.output(
+            "Replaced pkg preinstall script with our custom script at %s"
+            % our_script)
 
     def main(self):
         # Mount the image.
         mount_point = self.mount(self.env["dmg_path"])
-        temp_path = None
         # Wrap all other actions in a try/finally so the image is always
         # unmounted.
         try:
@@ -187,6 +193,5 @@ class AdobeReaderRepackager(DmgMounter):
 
 
 if __name__ == '__main__':
-    processor = AdobeReaderRepackager()
-    processor.execute_shell()
-        
+    PROCESSOR = AdobeReaderRepackager()
+    PROCESSOR.execute_shell()
--- AdobeReader/AdobeReaderURLProvider.py
+++ AdobeReader/AdobeReaderURLProvider.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""See docstring for AdobeReaderURLProvider class"""
 
 
 import json
@@ -24,7 +25,8 @@ from autopkglib import Processor, ProcessorError
 __all__ = ["AdobeReaderURLProvider"]
 
 
-AR_BASE_URL = ("http://get.adobe.com/reader/webservices/json/standalone/"
+AR_BASE_URL = (
+    "http://get.adobe.com/reader/webservices/json/standalone/"
     "?platform_type=Macintosh&platform_dist=OSX&platform_arch=x86-32"
     "&platform_misc=10.8.0&language=%s&eventname=readerotherversions")
 
@@ -34,7 +36,8 @@ MAJOR_VERSION_DEFAULT = "11"
 MAJOR_VERSION_MATCH_STR = "adobe/reader/mac/%s"
 
 class AdobeReaderURLProvider(Processor):
-    description = "Provides URL to the latest Adobe Reader release."
+    """Provides URL to the latest Adobe Reader release."""
+    description = __doc__
     input_variables = {
         "language": {
             "required": False,
@@ -57,42 +60,42 @@ class AdobeReaderURLProvider(Processor):
             "description": "URL to the latest Adobe Reader release.",
         },
     }
-    
-    __doc__ = description
-    
+
     def get_reader_dmg_url(self, base_url, language, major_version):
+        '''Returns download URL for Adobe Reader DMG'''
+        #pylint: disable=no-self-use
         request = urllib2.Request(base_url % language)
         request.add_header("x-requested-with", "XMLHttpRequest")
         try:
             url_handle = urllib2.urlopen(request)
             json_response = url_handle.read()
             url_handle.close()
-        except BaseException as e:
-            raise ProcessorError("Can't open %s: %s" % (base_url, e))
-            
+        except BaseException as err:
+            raise ProcessorError("Can't open %s: %s" % (base_url, err))
+
         reader_info = json.loads(json_response)
         major_version_string = MAJOR_VERSION_MATCH_STR % major_version
-        matches = [item["download_url"] for item in reader_info 
+        matches = [item["download_url"] for item in reader_info
                    if major_version_string in item["download_url"]]
         try:
             return matches[0]
         except IndexError:
             raise ProcessorError(
-                "Can't find Adobe Reader download URL for %s, version %s" 
+                "Can't find Adobe Reader download URL for %s, version %s"
                 % (language, major_version))
-    
+
     def main(self):
         # Determine base_url, language and major_version.
         base_url = self.env.get("base_url", AR_BASE_URL)
         language = self.env.get("language", LANGUAGE_DEFAULT)
         major_version = self.env.get("major_version", MAJOR_VERSION_DEFAULT)
-        
+
         self.env["url"] = self.get_reader_dmg_url(
             base_url, language, major_version)
         self.output("Found URL %s" % self.env["url"])
-    
+
 
 if __name__ == "__main__":
-    processor = AdobeReaderURLProvider()
-    processor.execute_shell()
-    
+    PROCESSOR = AdobeReaderURLProvider()
+    PROCESSOR.execute_shell()
+
