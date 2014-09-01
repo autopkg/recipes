ccd9594ee30693331188ff56638e801c6176af46
Matthias Choules <choules@wycomco.de>
2014-09-01 16:10:28 +0200

    Added support for non-english updates for Adobe Reader

--- AdobeReader/AdobeReaderURLProvider.py
+++ AdobeReader/AdobeReaderURLProvider.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright 2010 Per Olofsson
+# Copyright 2014 wycomco GmbH (choules@wycomco.de)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@
 
 import json
 import urllib2
+import ftplib
 
 from autopkglib import Processor, ProcessorError
 
@@ -34,6 +35,14 @@ LANGUAGE_DEFAULT = "English"
 MAJOR_VERSION_DEFAULT = "11"
 
 MAJOR_VERSION_MATCH_STR = "adobe/reader/mac/%s"
+
+MULTILANGUAGE_UPDATER = "false"
+
+AR_FTP_HOST = "ftp.adobe.com"
+
+AR_FTP_PATH = "/pub/adobe/reader/mac"
+
+AR_UPDATER_DOWNLOAD_URL = ("http://download.adobe.com/pub/adobe/reader/mac/%s.x/%s/misc/AdbeRdrUpd%s.dmg")
 
 class AdobeReaderURLProvider(Processor):
     """Provides URL to the latest Adobe Reader release."""
@@ -54,10 +63,18 @@ class AdobeReaderURLProvider(Processor):
             "required": False,
             "description": "Default is %s" % AR_BASE_URL,
         },
+        "multilanguage_updater": {
+            "required": False,
+            "description": ("Full version (English only) or updater (multilanguage). Examples: 'true', 'false'. Defaults to "
+                            "%s" % MULTILANGUAGE_UPDATER)
+        },
     }
     output_variables = {
         "url": {
             "description": "URL to the latest Adobe Reader release.",
+        },
+        "version": {
+            "description": "Version for this update.",
         },
     }
 
@@ -78,20 +95,59 @@ class AdobeReaderURLProvider(Processor):
         matches = [item["download_url"] for item in reader_info
                    if major_version_string in item["download_url"]]
         try:
-            return matches[0]
+            url = matches[0]
         except IndexError:
             raise ProcessorError(
                 "Can't find Adobe Reader download URL for %s, version %s"
                 % (language, major_version))
+
+        matches = [item["Version"] for item in reader_info
+                   if major_version_string in item["download_url"]]
+
+        try:
+            version = matches[0]
+        except IndexError:
+            raise ProcessorError(
+                "Can't find version information on Adobe Reader download URL for %s, version %s"
+                % (language, major_version))
+
+        return (url, version)
+
+    def get_reader_updater_dmg_url(self, language, major_version):
+        # Determine URL of multilanguage updater by browsing Adobe's public FTP server
+        '''Returns download URL for Adobe Reader Updater DMG'''
+        
+        try:
+            link=ftplib.FTP(AR_FTP_HOST)
+            link.login()
+            link.cwd("%s/%s.x" % (AR_FTP_PATH, major_version))
+            t=link.nlst()
+            link.quit()
+            version=t[-1]
+        except IndexError:
+            raise ProcessorError("Could not retrieve current version from FTP server")
+
+        versioncode = version.replace('.','')
+        url = AR_UPDATER_DOWNLOAD_URL % (major_version, version, versioncode)
+        
+        return (url, version)
 
     def main(self):
         # Determine base_url, language and major_version.
         base_url = self.env.get("base_url", AR_BASE_URL)
         language = self.env.get("language", LANGUAGE_DEFAULT)
         major_version = self.env.get("major_version", MAJOR_VERSION_DEFAULT)
+        multilanguage_updater = self.env.get("multilanguage_updater", MULTILANGUAGE_UPDATER)
 
-        self.env["url"] = self.get_reader_dmg_url(
-            base_url, language, major_version)
+        if multilanguage_updater == "true":
+            (url, version) = self.get_reader_updater_dmg_url(
+                language, major_version)
+        else:
+            (url, version) = self.get_reader_dmg_url(
+                base_url, language, major_version)
+
+        self.env["url"] = url
+        self.env["version"] = version
         self.output("Found URL %s" % self.env["url"])
 
 
