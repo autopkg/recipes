c4a6bacbba3a00ff24f0914efcd9159eb26999ba
Matt Hansen <hansen.m@me.com>
2015-08-06 15:57:03 -0400

    PyLint

--- Barebones/BarebonesURLProvider.py
+++ Barebones/BarebonesURLProvider.py
@@ -27,17 +27,18 @@ from autopkglib import Processor, ProcessorError
 __all__ = ["BarebonesURLProvider"]
 
 URLS = {"textwrangler": "https://versioncheck.barebones.com/TextWrangler.xml",
-       "bbedit": "https://versioncheck.barebones.com/BBEdit.xml"}
+        "bbedit": "https://versioncheck.barebones.com/BBEdit.xml"}
 
-# http://stackoverflow.com/a/24175862
 import ssl
 from functools import wraps
 def sslwrap(func):
-   @wraps(func)
-   def bar(*args, **kw):
-       kw['ssl_version'] = ssl.PROTOCOL_TLSv1
-       return func(*args, **kw)
-   return bar
+    """http://stackoverflow.com/a/24175862"""
+    @wraps(func)
+    def wraps_sslwrap(*args, **kw):
+        """Monkey-patch for sslwrap to force TLSv1"""
+        kw['ssl_version'] = ssl.PROTOCOL_TLSv1
+        return func(*args, **kw)
+    return wraps_sslwrap
 
 ssl.wrap_socket = sslwrap(ssl.wrap_socket)
 
