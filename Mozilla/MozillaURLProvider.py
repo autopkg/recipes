8332edca4fbafc1e8aff8c119e571f27b4853bb5
Greg Neagle <gregneagle@mac.com>
2015-11-16 22:16:59 -0800

    Update MozillaURLProvider, Firefox and Thunderbird recipes to handle new Mozilla download URLs

--- Mozilla/MozillaURLProvider.py
+++ Mozilla/MozillaURLProvider.py
@@ -27,9 +27,21 @@ from autopkglib import Processor, ProcessorError
 __all__ = ["MozillaURLProvider"]
 
 
-MOZ_BASE_URL = "http://ftp.mozilla.org/pub/mozilla.org"
-               #"firefox/releases")
-RE_DMG = re.compile(r'a[^>]* href="(?P<filepath>[^"]+\.dmg)"')
+MOZ_BASE_URL = "https://download.mozilla.org/?product=%s-%s&os=osx&lang=%s"
+
+# As of 16 Nov 2015 here are the supported products:
+# firefox-latest
+# firefox-esr-latest
+# firefox-beta-latest
+# thunderbird-latest
+# thunderbird-beta-latest
+# 
+# See also:
+#    http://ftp.mozilla.org/pub/firefox/releases/latest/README.txt
+#    http://ftp.mozilla.org/pub/firefox/releases/latest-esr/README.txt
+#    http://ftp.mozilla.org/pub/firefox/releases/latest-beta/README.txt
+#    http://ftp.mozilla.org/pub/thunderbird/releases/latest/README.txt
+#    http://ftp.mozilla.org/pub/thunderbird/releases/latest-beta/README.txt
 
 
 class MozillaURLProvider(Processor):
@@ -43,14 +55,16 @@ class MozillaURLProvider(Processor):
         },
         "release": {
             "required": False,
+            "default": 'latest',
             "description": (
                 "Which release to download. Examples: 'latest', "
-                "'latest-10.0esr', 'latest-esr', 'latest-beta'."),
+                "'esr-latest', 'beta-latest'. Defaults to 'latest'"),
         },
         "locale": {
             "required": False,
+            "default": 'en-US',
             "description":
-                "Which localization to download, default is 'en_US'.",
+                "Which localization to download, default is 'en-US'.",
         },
         "base_url": {
             "required": False,
@@ -64,68 +78,26 @@ class MozillaURLProvider(Processor):
     }
 
     def get_mozilla_dmg_url(self, base_url, product_name, release, locale):
-        """Get download URL for Mozilla product"""
+        """Assemble download URL for Mozilla product"""
         #pylint: disable=no-self-use
         # Allow locale as both en-US and en_US.
         locale = locale.replace("_", "-")
+        
+        # fix releases into new format
+        if release == 'latest-esr':
+            release = 'esr-latest'
+        if release == 'latest-beta':
+            release = 'beta-latest'
 
-        # Construct download directory URL.
-        release_dir = release.lower()
-
-        index_url = "/".join(
-            (base_url, product_name, "releases", release_dir, "mac", locale)) + '/'
-        #print >>sys.stderr, index_url
-
-        # Read HTML index.
-        try:
-            fref = urllib2.urlopen(index_url)
-            html = fref.read()
-            fref.close()
-        except BaseException as err:
-            raise ProcessorError("Can't download %s: %s" % (index_url, err))
-
-        # Search for download link.
-        matches = RE_DMG.findall(html)
-        if len(matches):
-            def compare_version(this, that):
-                """Compare loose versions"""
-                return cmp(
-                    LooseVersion(parse_version_from_path(this)),
-                    LooseVersion(parse_version_from_path(that))
-                )
-            def parse_version_from_path(path):
-                path = urllib.unquote(path)
-                name = path.split('/')[-1]
-                version = re.search('[0-9]([0-9a-zA-Z_-]*)(\.[0-9][0-9a-zA-Z_-]*)*', name)
-                if version:
-                    return version.group(0)
-                # if no version found, just return filename
-                return name
-
-            sorted_items = sorted(matches, cmp=compare_version)
-            filepath = sorted_items[-1]
-        else:
-            filepath = None
-        if not filepath:
-            raise ProcessorError(
-                "Couldn't find %s download URL in %s"
-                % (product_name, index_url))
-
-        # Return URL.
-        if '/' == filepath[0]:
-            # absolute link URL
-            return urlparse.urljoin(base_url, filepath)
-        else:
-            return "/".join(
-                (base_url, product_name, "releases", release_dir, "mac", locale,
-                 filepath))
+        # Construct download URL.
+        return base_url % (product_name, release, locale)
 
     def main(self):
         """Provide a Mozilla download URL"""
         # Determine product_name, release, locale, and base_url.
         product_name = self.env["product_name"]
         release = self.env.get("release", "latest")
-        locale = self.env.get("locale", "en_US")
+        locale = self.env.get("locale", "en-US")
         base_url = self.env.get("base_url", MOZ_BASE_URL)
 
         self.env["url"] = self.get_mozilla_dmg_url(
