bed17b77538fccaf1dd33f2640561dc568dc6b9e
Nathan Felton <n8felton@gmail.com>
2022-05-27 16:45:46 -0400

    It is actually 2022, not 2021...

--- AdobeReader/AdobeReaderURLProvider.py
+++ AdobeReader/AdobeReaderURLProvider.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #
 # Copyright 2010 Per Olofsson
-#           2021 Nate Felton <n8felton>
+#           2022 Nate Felton <n8felton>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
